e487e9310469cf751f81d3b109bc511aefa1d4f2
NmnSach <nssachdeva2@gmail.com>
2024-06-11 00:42:39 +0530

    People component

--- frontend/app/alumnus/page.jsx
+++ frontend/app/alumnus/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import People from '../components/People'
 
 const page = () => {
     return (
-        <div>
-            Alumnus
+        <div className='flex flex-col justify-center items-center mt-24'>
+            <div>
+                <h1 className='text-2xl sm:text-4xl font-bold my-5 underline decoration-yellow-400'>Meet our Alumni</h1>
+            </div>
+
+            <div className='w-10/12'>
+                <People
+                    imagepath="/prof.jpg"
+                    name="Shaheer Khan"
+                    description="I completed my PhD on the movement ecology of Indian wolves in a 
+                    human dominated landscape. My primary interests lie in large carnivore ecology, 
+                    movement ecology, urban wildlife, and the impact of linear infrastructure on wildlife 
+                    ecology."
+                />
+            </div>
         </div>
     )
 }
--- frontend/app/components/People.jsx
+++ frontend/app/components/People.jsx
@@ -3,18 +3,22 @@ import React from 'react'
 
 const People = ({ imagepath, name, description }) => {
     return (
-        <div>
+        <div className='flex '>
 
             <Image
                 src={imagepath}
-                width={400}
-                height={400}
+                width={250}
+                height={250}
                 alt='person-image'
-                className='rounded-full p-2'
+                className='rounded-full'
             />
 
-            <h1 className='text-2xl font-bold'>{name}</h1>
-            <p>{description}</p>
+            <div className='flex justify-center items-start flex-col ml-10'>
+
+                <h1 className='text-2xl font-bold'>{name}</h1>
+                <p>{description}</p>
+
+            </div>
 
         </div>
     )

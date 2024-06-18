783afaa52ca261070f743ac14bc8895cd7d3caf7
NmnSach <nssachdeva2@gmail.com>
2024-06-18 21:46:16 +0530

    .

--- frontend/app/research/page.jsx
+++ frontend/app/research/page.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import CurrentProjects from '../components/CurrentProjects'
 import CompletedProjects from '../components/CompletedProjects'
+import ResearchJournalPapers from '../components/ResearchJournalPapers'
+import PublishedBooksAuthored from '../components/PublishedBooksAuthored'
+import PublishedBooksEdited from '../components/PublishedBooksEdited'
 
 
 const Research = () => {
     return (
         <div className='mt-20'>
             <CurrentProjects />
-
             <CompletedProjects />
+            <ResearchJournalPapers />
+            <PublishedBooksAuthored />
+            <PublishedBooksEdited />
         </div>
     )
 }
